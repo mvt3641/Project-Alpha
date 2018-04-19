170551650745e1a2ea236b86ad86566a86a9b9b7
Matt <mvt3641@gmail.com>
2018-04-18 20:44:09 -0400

    need to update paths

---
+++ controllers/api.js
@@ -0,0 +1,106 @@
+//requiring express for routing and server services
+var express = require('express');
+var mongoose =require('mongoose');
+// Import models
+//var ----- = require('../models/ ex.flightmodel')
+//requring the mongodb database connection
+  var db = require('../config/connection');
+mongoose.Promise = Promise;
+//creating a router for export to handle middleware and routing
+var router = express.Router();
+
+//get all data from the mysql table
+router.post('/flightdata',function(req,res){
+
+
+  //Since this in an uploaded file, sort by _id of upload
+flight.find(searchmon).sort({"_id":1}).exec(function(err,results){
+    if(err) throw err;
+    res.json(results)
+      console.log(`${results.length}  files returned on query`)
+
+  });
+
+});
+
+//Api test route//
+//Route for getting ground winds
+router.get('/api/flightdata/chart', function(req,res){
+  // db.query('SELECT GROUND_,TIME,Date,Winds_Aloft,TENSION FROM asitrep;',function(err,results){
+  //   if(err) throw err;
+  //   res.json(results);
+  // })
+});
+
+
+// Route for getting date and time from table
+router.get('/api/flightdata/data', function(req,res){
+  // db.query('SELECT Date, TIME FROM asitrep;',function(err,results){
+  //   if(err) throw err;
+  //   res.json(results);
+  // })
+});
+
+
+// Route to grab data from client-side and add to mysql table.
+router.post('/api/flightdata',function(req,res){
+  // db.query('INSERT INTO asitrep SET ?;', req.body,function(err,results){
+  //   if(err) throw err;
+  //   //// insert here command to add to Chart
+  //   ////
+  // })
+  //   db.query('SELECT * FROM aisitrep;', function(req,res){
+  //     res.json(res)
+  //   })
+ });
+
+
+
+/////////////////////// PAX TRACKER /////////////////////////////////////////////////////////
+
+router.post('/api/pax', function(req,res){
+  db.query('INSERT INTO paxtracker SET ?', req.body, function(err,results){
+    if(err) throw err;
+    console.log(req.body);
+  //response to client side
+  db.query('SELECT * FROM paxtracker', function(err, results) {
+            if (err) throw err;
+            res.json(results);
+  })
+})
+});
+
+
+////show the whole roster////
+router.get('/api/pax',function(req,res){
+  db.query('SELECT * FROM paxtracker', function(err, results) {
+            if (err) throw err;
+            res.json(results);
+  })
+})
+
+/////////SITE ROUTE//////////////////
+
+router.post('/api/site',function(req,res){
+db.query('INSERT INTO location SET ?', req.body, function(err,results){
+    if(err) throw err;
+    console.log(req.body);
+    //response to client side
+    db.query('SELECT * FROM location', function(err, results) {
+              if (err) throw err;
+              res.json(results);
+    })
+})
+});
+
+
+router.get('/api/site', function(req,res){
+  db.query('SELECT * FROM location', function(err,results){
+    if(err) throw err;
+    res.json(results);
+  })
+});
+
+
+
+module.exports = router;
--- server.js
+++ server.js
@@ -21,7 +21,7 @@ app.use(logger("dev"));
 app.use(bodyParser.urlencoded({ extended: true }));
 // parse application/json
 app.use(bodyParser.json());
-app.use(apiRoutes);
+//app.use(apiRoutes);
 
 app.use('/', express.static(path.join(__dirname, 'public')));
 
