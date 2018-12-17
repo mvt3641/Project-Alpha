65b36849123a0d647e8a60c0e91a1ed946e6950f
Matt <mvt3641@gmail.com>
2018-12-16 23:55:14 -0500

    added random dropdown selection, need to adjust for chronological automation

--- test.js
+++ test.js
@@ -3,6 +3,12 @@ var Nightmare = require("nightmare");
 var nightmare = new Nightmare({ show: true,typeInterval: 200
  });
 
+ var flightStatus = ["FMC","PMC","NMC"];
+
+ var reasonMoored = ["WX","SM","UM","BD","Aloft"];
+
+ var names = ['Woody','Leo','Gunner','Nick','Crawley','Sergi'];
+
  function random(max) {
  return Math.floor(Math.random() * Math.floor(max));
 }
@@ -11,13 +17,13 @@ function getRnd(min, max) {
   return Math.floor(Math.random() * (max - min) ) + min;
 }
 
-
-var names = ['Woody','Leo','Gunner','Nick','Crawley','Sergi'];
-
-function randomnames(array){
-   let randName= Math.floor(Math.random() * array.length -1)
-  return array[randName];
+function randomarr(array){
+   let rand= Math.floor(Math.random() * array.length -1)
+  return array[rand];
 }
+
+var rdmstatus = Math.floor(Math.random()*flightStatus.length);
+var rdmmooring = Math.floor(Math.random()*reasonMoored.length);
 
 //randomnames(names);
 
@@ -27,30 +33,30 @@ nightmare
   .type("#exampleInputEmail3","test@gmail.com")
   .type("#exampleInputPassword3", "testtest")
   .click("#login")
-  .click(href="flightTab")
-  // .click("#flightTab")
+  .wait(2000)
+  .click("#FlightTab")
   .wait(6000)
   .click("#addlog")
   .type("#dateLogged","2018-12-"+random(20))
-  .type("#userLogged", randomnames(names))
-  .type("#TimeLogged","500")
-  .type("aerostatSN", "Site 1")
+  .type("#userLogged", randomarr(names))
+  .type("#TimeLogged","0500")
+  .type("#aerostatSN", "Site 1")
   .evaluate(function(selector, value) {
    jQuery(selector).val(value);
-}, '#System-Status', random())
-   .type("#Flight-Altitude-Input", random(1500,2000))
+}, '#System-Status', flightStatus[rdmstatus])
+   .type("#Flight-Altitude-Input", getRnd(1500,2000))
    .evaluate(function(selector, value) {
     jQuery(selector).val(value);
- }, '#Reason-Moored-Input', random())
-   .type("#Launches-Input", random(1,2))
-   .type("#Recoveries-Input", random(1,2))
-   .type("#Tether-Tension-input", random(750,1100))
-   .type("#Ground-Winds-Input", random(8,20))
-   .type("#Winds-Aloft-Input", random(8,20))
+ }, '#Reason-Moored-Input', reasonMoored[rdmmooring])
+   .type("#Launches-Input", getRnd(1,2))
+   .type("#Recoveries-Input", getRnd(1,2))
+   .type("#Tether-Tension-Input", getRnd(750,1100))
+   .type("#Ground-Winds-Input", getRnd(8,20))
+   .type("#Winds-Aloft-Input", getRnd(8,20))
    .type('#Notes', "Name the automation")
    .click('#saved')
    .click("#metricsTab")
-   .goto("operationUI.html")
+   .click("#operationsPage")
   .then(function(res) {
     console.log("Confirmed");
   })
