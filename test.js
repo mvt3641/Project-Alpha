18547b71641572a78ce86972dcb0d30ab5e4f9bd
Matt <mvt3641@gmail.com>
2018-12-17 21:39:11 -0500

    updated test.js

--- test.js
+++ test.js
@@ -1,6 +1,6 @@
 var Nightmare = require("nightmare");
 
-var nightmare = new Nightmare({ show: true,typeInterval: 200
+var nightmare = new Nightmare({ show: true,typeInterval: 100
  });
 
  var flightStatus = ["FMC","PMC","NMC"];
@@ -8,6 +8,10 @@ var nightmare = new Nightmare({ show: true,typeInterval: 200
  var reasonMoored = ["WX","SM","UM","BD","Aloft"];
 
  var names = ['Woody','Leo','Gunner','Nick','Crawley','Sergi'];
+
+ var notesArray = ["Launched Aerostat","Performed maintenance on platform","Inflated to 7MC", "Moored for Weather", "Patched Aerostat", "Launching at 0700"]
+
+
 
  function random(max) {
  return Math.floor(Math.random() * Math.floor(max));
@@ -24,8 +28,9 @@ function randomarr(array){
 
 var rdmstatus = Math.floor(Math.random()*flightStatus.length);
 var rdmmooring = Math.floor(Math.random()*reasonMoored.length);
-
+var rdmnotes = Math.floor(Math.random()*notesArray.length);
 //randomnames(names);
+
 
 
 nightmare
@@ -39,7 +44,7 @@ nightmare
   .click("#addlog")
   .type("#dateLogged","2018-12-"+random(20))
   .type("#userLogged", randomarr(names))
-  .type("#TimeLogged","0500")
+  .type("#TimeLogged",getRnd(1,12))
   .type("#aerostatSN", "Site 1")
   .evaluate(function(selector, value) {
    jQuery(selector).val(value);
@@ -53,7 +58,7 @@ nightmare
    .type("#Tether-Tension-Input", getRnd(750,1100))
    .type("#Ground-Winds-Input", getRnd(8,20))
    .type("#Winds-Aloft-Input", getRnd(8,20))
-   .type('#Notes', "Name the automation")
+   .type('#Notes', notesArray[rdmnotes])
    .click('#saved')
    .click("#metricsTab")
    .click("#operationsPage")
